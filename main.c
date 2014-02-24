f4c12389844029b1032619f5cbb61fe267bca67a
bbarba <bbarba9524@gmail.com>
2014-02-23 22:00:35 -0700

    Update main.c
    This is the template.

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
-#ifndef CSE220_project2_common_h
-#define CSE220_project2_common_h
+#include "common.h"
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <string.h>
-#include <time.h>
+int main (int argc, const char *argv[])
+{
+    FILE *source_file;
+    char source_name[MAX_FILE_NAME_LENGTH];
+    char date[DATE_STRING_LENGTH];
+    
+   /* Missing Code Here */
+    return 0;
+}
+FILE *init_lister(const char *name, char source_file_name[], char dte[])
+{
+    time_t timer;
+    FILE *file;
+    
+    /* Missing Code Here */
+    return file;
+}
+BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
+{
+    char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
+    char source_buffer[MAX_SOURCE_LINE_LENGTH];
+    static int line_number = 0;
+    
+    if (1) //This is missing a condition
+    {
+		/*  Missing Code Here */
+        return (TRUE);
+    }
+    else
+    {
+        return (FALSE);
+    }
+}
 
-#define FORM_FEED_CHAR          '\f'
-#define MAX_FILE_NAME_LENGTH    32
-#define MAX_SOURCE_LINE_LENGTH  256
-#define MAX_PRINT_LINE_LENGTH   80
-#define MAX_LINES_PER_PAGE      50
-#define DATE_STRING_LENGTH      26
-
-typedef enum {
-    FALSE, TRUE,
-}BOOLEAN;
-
-FILE *init_lister(const char *, char source_file_name[], char dte[]);
-BOOLEAN get_source_line(FILE *, char src_name[], char todays_date[]);
-void print_line(char *, char source_name[], char date_to_print[]);
-
-#endif
