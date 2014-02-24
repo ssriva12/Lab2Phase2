e9105cbb14aa0d71a750da86531bad3bbca53200
bbarba <bbarba9524@gmail.com>
2014-02-23 22:26:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main (int argc, const char *argv[])
     
     strcpy(source_name, argv[1]); //fetch name as argv[1]
     source_file = init_lister(source_name, source_name, date); //passing it to function init_lister, and copying name into source_name
+    
+    while(true) //while loop
+    {
+    	get_source_line(source_file, source_name, date); //boolean function to read and print source files
+    }
     return 0;
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
